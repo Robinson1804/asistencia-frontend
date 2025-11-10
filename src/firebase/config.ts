237986767f29fabd75d8bb9cb9a29b6d453cf9d5
Robinson Cerron Mercado <dr.cerronmercado@gmail.com>
2025-11-10 09:42:11 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyC...Y",
-  authDomain: "control-de-acceso-ca993.firebaseapp.com",
-  projectId: "control-de-acceso-ca993",
-  storageBucket: "control-de-acceso-ca993.appspot.com",
-  messagingSenderId: "493...7",
-  appId: "1:493...b7",
+  "projectId": "control-de-acceso-ca993",
+  "appId": "1:736558720317:web:dbf55b0ca1954dbc841154",
+  "apiKey": "AIzaSyC0I-76AUTMQtNRYDNdsTdfPnydQ9Ktc-s",
+  "authDomain": "control-de-acceso-ca993.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "736558720317"
 };
